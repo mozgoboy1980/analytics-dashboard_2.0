fcac889ae1aac2c7c57130247b935c33e86cf41f
mozgoBoy1980 <terekhov.digital@email.com>
2025-07-23 23:25:12 +0300

    fix: добавлены аргументы start, end, metrics в fetch_ga4_data

--- data/ga4_collector.py
+++ data/ga4_collector.py
@@ -1,17 +1,20 @@
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunReportRequest, DateRange, Dimension, Metric
 from google.oauth2 import service_account
-from config.settings import GA4_PROPERTY_ID, START_DATE, END_DATE, SERVICE_ACCOUNT_FILE
+from config.settings import GA4_PROPERTY_ID, SERVICE_ACCOUNT_FILE
 
-def fetch_ga4_data():
+def fetch_ga4_data(start_date: str, end_date: str, metric_names: list):
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
     client = BetaAnalyticsDataClient(credentials=credentials)
 
+    dimensions = [Dimension(name="pageTitle"), Dimension(name="pagePath")]
+    metrics = [Metric(name=m) for m in metric_names]
+
     request = RunReportRequest(
         property=f"properties/{GA4_PROPERTY_ID}",
-        dimensions=[Dimension(name="pageTitle"), Dimension(name="pagePath")],
-        metrics=[Metric(name="activeUsers"), Metric(name="engagementRate")],
-        date_ranges=[DateRange(start_date=START_DATE, end_date=END_DATE)]
+        dimensions=dimensions,
+        metrics=metrics,
+        date_ranges=[DateRange(start_date=start_date, end_date=end_date)]
     )
 
     response = client.run_report(request)
